6f2f6813a52b44d69d459a8d36f282fb2e230234
Dan E <dhengland44@gmail.com>
2024-07-28 16:07:35 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use std::env;
 mod libs;
+use home::home_dir;
+
 fn main() {
-    let arguments = env::args().collect::<Vec<String>>();
-    let len = arguments.len() as i32;
-    if (len < 2) || (arguments[1] == "list".to_string()) {
-        libs::output_tasks();
-    } else if arguments[1] == "add".to_string() {
-        libs::add(arguments, len)
-    } else if arguments[1] == "remove".to_string() {
-        libs::remove(arguments)
+    if let Some(home_path) = home_dir() {
+        if let Err(e) = env::set_current_dir(&home_path) {
+            eprintln!("Failed to change the working directory: {}", e);
+            return;
+        }
+
+        let arguments: Vec<String> = env::args().collect();
+        let len = arguments.len();
+
+        if len < 2 || arguments[1] == "list" {
+            if let Err(e) = libs::output_tasks() {
+                eprintln!("Error displaying tasks: {}", e);
+            }
+        } else if arguments[1] == "add" {
+            if let Err(e) = libs::add(&arguments) {
+                eprintln!("Error adding task: {}", e);
+            }
+        } else if arguments[1] == "remove" {
+            if let Err(e) = libs::remove(&arguments) {
+                eprintln!("Error removing task: {}", e);
+            }
+        } else {
+            eprintln!("Invalid command");
+        }
+    } else {
+        eprintln!("Could not find the home directory");
     }
 }
